8d2a3b2333c74ab669061a445ebb6b8169fa6f01
diljitehg <diljit807singh@gmail.com>
2024-02-15 23:58:18 -0500

    Add project files.

---
+++ PointOfSale/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace PointOfSale
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
