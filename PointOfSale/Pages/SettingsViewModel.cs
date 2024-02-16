149ab65028a13834317ac3e13911a660c204dcb4
gourav263587 <gouravmaallah@gmail.com>
2024-02-16 00:15:29 -0500

    Created settings page where user can edit the product details

---
+++ PointOfSale/Pages/SettingsViewModel.cs
@@ -0,0 +1,31 @@
+﻿namespace PointOfSale.Pages;
+using PointOfSale.Pages;
+[INotifyPropertyChanged]
+public partial class SettingsViewModel
+{
+    [ObservableProperty]
+    ObservableCollection<Item> _products;
+
+    public SettingsViewModel()
+    {
+        _products = new ObservableCollection<Item>(
+            AppData.Items
+        );
+    }
+
+    [RelayCommand]
+    async Task NotImplemented()
+    {
+        if (Shell.Current != null && Shell.Current.Navigation != null)
+        {
+            await Shell.Current.GoToAsync("//OrdersViewPage");
+        }
+        else
+        {
+            
+            await App.Current.MainPage.DisplayAlert("Not Implement Yet", "FYP", "OK");
+        }
+    }
+}
+
+
