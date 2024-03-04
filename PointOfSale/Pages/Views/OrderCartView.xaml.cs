a9ea2e1d0ef6010082da6f3d5648416e7d25ec62
diljitehg <diljit807singh@gmail.com>
2024-03-03 23:22:50 -0500

    Added Ordercart view where user can add product to cart

---
+++ PointOfSale/Pages/Views/OrderCartView.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows.Input;
+
+namespace PointOfSale.Pages.Views
+{
+    public partial class OrderCartView : ContentView
+    {
+        public ICommand PlaceOrderCommand { get; }
+
+        public OrderCartView()
+        {
+            InitializeComponent();
+            PlaceOrderCommand = new Command(ExecutePlaceOrderCommand);
+        }
+
+        private async void ExecutePlaceOrderCommand()
+        {
+            await Shell.Current.GoToAsync("//PointOfSale.Pages.Handheld.OrdersPage");
+        }
+    }
+}
---
+++ PointOfSale/Pages/Views/OrderCartViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace PointOfSale.Pages.Views;
+
+[INotifyPropertyChanged]
+public partial class OrderCartViewModel
+{
+    [ObservableProperty]
+    Order order;
+
+    public OrderCartViewModel()
+    {
+        Order = AppData.Orders.First();
+    }
+
+    [RelayCommand]
+    async Task PlaceOrder()
+    {
+     //   await Shell.Current.GoToAsync("//PointOfSale.Pages.Handheld.OrdersPage");
+        await App.Current.MainPage.DisplayAlert("Not Implemented", "Just for FYP", "Okay");
+    }
+}
